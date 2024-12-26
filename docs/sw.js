54f55b11bf611a14311f6779765a0d16f792c034
grapes <Syrovatskiy.Gleb@wildberries.ru>
2024-12-27 00:05:26 +0300

    init

---
+++ docs/sw.js
@@ -0,0 +1,41 @@
+const CACHE_NAME = "v1_cache_panel_adm",
+  urlsToCache = [
+    "window.pwaUrl",
+    "./manifest.json",
+  ];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(
+    caches
+      .open(CACHE_NAME)
+      .then((cache) => {
+        return cache.addAll(urlsToCache).then(() => self.skipWaiting());
+      })
+      .catch((err) => "fallo el registro del cache ", err)
+  );
+});
+
+self.addEventListener("activate", (e) => {
+  const caheWitheList = [CACHE_NAME];
+  e.waitUntil(
+    caches.keys().then((cachesNames) =>
+      cachesNames.map((cacheName) => {
+        console.log(cacheName);
+        if (cacheName.indexOf(cacheName) === -1) {
+          return caches.delete(cacheName);
+        }
+      })
+    )
+  );
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then((res) => {
+      if (res) {
+        return res;
+      }
+      return fetch(e.request);
+    })
+  );
+});
